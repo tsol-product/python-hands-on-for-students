34dc6fa04c65cfdcf56bd47d51c1011d7a2bb33b
TAoyama2 <aoyama@tsolpd.onmicrosoft.com>
2018-11-08 16:24:20 +0900

    first commit

---
+++ webApplication_flask/flaskr/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('flaskr.config')
+
+db = SQLAlchemy(app)
+
+import flaskr.views
---
+++ webApplication_flask/flaskr/config.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskr.db'
+SECRET_KEY = 'U\xec\xb0X\xe1\xe6\xd3\xf5\xbb\xe9\x1f\x1f:\x00\xbb\xf5\xaa\xc7V\xf2_\x9f\xa2}'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
