87cb501192722d26bc656bdfadf55b5241eebcf8
TAoyama2 <aoyama@tsolpd.onmicrosoft.com>
2019-01-31 14:24:30 +0900

    change config.py

--- webApplication_flask/flaskr/config.py
+++ webApplication_flask/flaskr/config.py
@@ -3,5 +3,5 @@
 # DB に SQLite を使用し, flaskr.db というファイル名で扱う
 SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskr.db'
 # DB接続のために必要
-SECRET_KEY = 'U\xec\xb0X\xe1\xe6\xd3\xf5\xbb\xe9\x1f\x1f:\x00\xbb\xf5\xaa\xc7V\xf2_\x9f\xa2}'
+SECRET_KEY = 'add your key'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
